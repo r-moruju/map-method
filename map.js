27cc8f35c942ed2a5cb96478f458f7de7b13bd6c
R Moruju <morujurazvan@gmail.com>
2022-10-30 08:35:10 +0000

    All done

--- map.js
+++ map.js
@@ -5,17 +5,25 @@
  */
 
 // Using a for loop
-
-
+let nums = [1, 2, 3, 4, 5];
+let upNums =[];
+for (let num of nums) {
+  upNums.push(num*2);
+}
+//console.log(upNums);
 // Using map()
-
+const callBackFunction = function(num) {
+  return num * 2
+}
+let upNumsMap = nums.map(callBackFunction)
+//console.log(upNumsMap)
 
 // Simplified w/ map()
 
 
 // Simplfied w/ map() + arrow function
-
-
+const simpleMap = nums.map(num => num*2);
+//console.log(simpleMap);
 // With objects:
 const students = [
   {
@@ -37,3 +45,27 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const selectId = students.map(student => [student.id, student.name]);
+//console.log(selectId);
+
+students.map(student => {
+  if (student.name == 'Ariel') {
+    student.age = 31;
+  }
+  if (student.name == 'Jason') {
+    student.age = 33;
+  }
+  if (student.name == 'Mark') {
+    student.age = 40;
+  }
+});
+//console.log(students);
+
+const selectMark = students.map(function(student){
+  if (student.name == 'Mark'){
+    return {student}
+  }
+})
+
+console.log(selectMark);
